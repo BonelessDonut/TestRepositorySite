40b8a037e82024e6f982d4529ec49e8e23b48205
BonelessDonut <108380371+BonelessDonut@users.noreply.github.com>
2024-07-26 23:37:16 -0400

    Created some file structure and set up some basic files to begin creating the game.

---
+++ src/classes/Overworld.js
@@ -0,0 +1,22 @@
+class Overworld {
+    constructor(){
+        const canvas = document.querySelector(".game-canvas");
+        const ctx = canvas.getContext("2d");
+    }
+
+    startGameLoop(){
+
+    }
+
+    bindActionInput(){
+        
+    }
+
+    startMap(mapConfig){
+
+    }
+
+    init(){
+
+    }
+}
